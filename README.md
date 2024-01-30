c87522ca31c7d7ab819e1038b0f851f883ae7d54
felix <chentengfei1998@gmail.com>
2024-01-30 22:46:01 +0800

    placehold

---
+++ README.md
@@ -0,0 +1,2 @@
+refer https://www.bilibili.com/video/BV1DM41137MW/?p=2&spm_id_from=pageDriver&vd_source=f48dec01ce8030e682cc2379c5a44020
+
---
+++ init.lua
@@ -0,0 +1,50 @@
+local set = vim.o
+
+set.number = true
+set.relativenumber = true
+
+set.clipboard = "unnamed"
+
+
+vim.api.nvim_create_autocmd({"TextYankPost"},{
+	pattern = {"*"},
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+
+local opt = {noremap = true, silent = true}
+vim.g.mapleader = " "
+vim.keymap.set("n","<C-l>","<C-w>l",opt)
+vim.keymap.set("n","<C-k>","<C-w>k",opt)
+vim.keymap.set("n","<C-j>","<C-w>j",opt)
+vim.keymap.set("n","<C-h>","<C-w>h",opt)
+vim.keymap.set("n","<Leader>v","<C-w>v",opt)
+vim.keymap.set("n","<Leader>s","<C-w>s",opt)
+vim.keymap.set("n","j",[[v:count ? 'j' : 'gj']], {noremap = true, expr = true})
+vim.keymap.set("n","k",[[v:count ? 'k' : 'gk']], {noremap = true, expr = true})
+
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	"RRethy/nvim-base16",
+	lazy = true,
+})
+
+vim.cmd.colorscheme("base16-tender")
